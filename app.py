335d0a82771df22eb3430e9feac1607265d20056
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2022-01-18 11:51:23 +0530

    added requirements.txt

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
   def get(self, name):
-    return {"student": name}
+    item = next(filter(lambda x: x["name"] == name, items), None)
+    return {"item": item}, 200 if item else 404
 
-api.add_resource(Student, '/student/<string:name>')   # http://127.0.0.1:5000/student/Bob
+  def post(self, name):
+    # if there already exists an item with "name", show a messege, and donot add the item
+    if next(filter(lambda x: x["name"] == name, items), None):
+      return {"messege": f"item {name} already exists"} ,400
 
-app.run(port=5000)
+    data = request.get_json()   # get_json(force=True) means, we don't need a content type header
+    item = {                    # flask will look into the content, and format the data.
+      "name": name,
+      "price": data["price"]
+    }
+    items.append(item)
+    return item, 201  # 201 is for CREATED status
+
+class ItemList(Resource):
+  def get(self):
+    return {"items": items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
