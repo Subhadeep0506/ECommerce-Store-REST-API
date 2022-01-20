c8007f1e975a538dc87414f072a247bc21a70db4
Subhadeep <subhadeepdoublecap@gmail.com>
2022-01-20 20:46:20 +0530

    added database and user registration

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 
@@ -91,5 +92,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
--- security.py
+++ security.py
@@ -1,20 +1,12 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-  User(1, "bob", "1234")
-]
-
-username_mapping = {u.username: u for u in users}   # mapping the users using set comprehension
-
-userid_mapping = {u.id: u for u in users}
-
 def authenticate(username, password):
-  user = username_mapping.get(username, None)
+  user = User.find_by_username(username)
   # if user and user.password == password:
   if user and safe_str_cmp(user.password, password):
     return user
 
 def identity(payload):  # payload is the contents of jwt
-  userid = payload["identity"]
-  return userid_mapping.get(userid, None)
+  user_id = payload["identity"]
+  return User.find_by_id(user_id)
