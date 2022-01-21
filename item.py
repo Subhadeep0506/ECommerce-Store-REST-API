281696e46f154ef779b2c8e3de122c49e81257e0
Subhadeep <subhadeepdoublecap@gmail.com>
2022-01-21 17:00:31 +0530

    implemeted delete method

--- item.py
+++ item.py
@@ -13,15 +13,6 @@ class Item(Resource):
     help="This field cannot be blank"
   )
   
-  # TO GET ITEM WITH NAME
-  @jwt_required()
-  def get(self, name):
-    item = self.find_item_by_name(name)
-    if item:
-      return item
-    
-    return {"message": "item not found."}, 404
-
   # searches the database for items using name
   @classmethod
   def find_item_by_name(cls, name):
@@ -42,6 +33,27 @@ class Item(Resource):
             }
         }
 
+  # method to insert an item into
+  @classmethod
+  def insert(cls, item):
+    connection = sqlite3.connect("./test/data.db")
+    cursor = connection.cursor()
+
+    query = "INSERT INTO items VALUES (?, ?)"
+    cursor.execute(query, (item["name"], item["price"]))
+
+    connection.commit()
+    connection.close()
+
+  # TO GET ITEM WITH NAME
+  @jwt_required()
+  def get(self, name):
+    item = self.find_item_by_name(name)
+    if item:
+      return item
+    
+    return {"message": "item not found."}, 404
+
   # TO POST AN ITEM
   def post(self, name):
     # if there already exists an item with "name", show a messege, and donot add the item
@@ -54,32 +66,31 @@ class Item(Resource):
       "name": name,
       "price": data["price"]
     }
+
+    try:
+      self.insert(item)
+    except:
+      return {"messege": "An error occured."}
     
-    connection = sqlite3.connect("./test/data.db")
-    cursor = connection.cursor()
-
-    query = "INSERT INTO items VALUES (?, ?)"
-    cursor.execute(query, (item["name"], item["price"]))
-
-    connection.commit()
-    connection.close()
-
     return item, 201  # 201 is for CREATED status
 
   # TO DELETE AN ITEM
   def delete(self, name):
-    # use the items variable avalable globally
-    global items
-    # check if the item exists
-    item = next(filter(lambda x: x["name"] == name ,items), None)
+    # check if there exists any item by name: "name"
+    # if exists then delete it
+    if self.find_item_by_name(name):
+      connection = sqlite3.connect("./test/data.db")
+      cursor = connection.cursor()
+
+      query = "DELETE FROM items WHERE name=?"
+      cursor.execute(query, (name,))
+
+      connection.commit()
+      connection.close()
+      return {"messege": "Item deleted"}
+
     # if doesn't exist, skip deleting
-    if item is None:
-      return {"messege": "Item don't exist"}, 400
-    
-    # store all the elements of items variable into local item
-    # except the one that was selected to be deleted
-    items = list(filter(lambda x: x["name"] != name, items))
-    return {"messege": "Item deleted"}
+    return {"messege": "Item don't exist"}, 400
 
   # TO ADD OR UPDATE AN ITEM
   def put(self, name):
@@ -103,4 +114,14 @@ class ItemList(Resource):
 
   # TO GET ALL ITEMS
   def get(self):
+    items = []
+    connection = sqlite3.connect("./test/data.db")
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM items"
+    for row in cursor.execute(query):
+      items.append({"name": row[0], "price": row[1]})
+
+    connection.commit()
+    connection.close()
     return {"items": items}
--- test/test.py
+++ test/test.py
@@ -1,17 +1,17 @@
 import sqlite3
 
-connection = sqlite3.connect('./test/data.db')
+connection = sqlite3.connect('./test/test.db')
 
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS users (id int PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
-user1 = (1, "bob", "1234")
+# user1 = (1, "bob", "1234")
 
-insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+# insert_query = "INSERT INTO users VALUES (?, ?, ?)"
 
-cursor.execute(insert_query, user1)
+# cursor.execute(insert_query, user1)
 
 users = [
   (2, "rolf", "2345"),
@@ -20,11 +20,13 @@ users = [
   (5, "clint", "5678")
 ]
 
-cursor.executemany(insert_query, users)
+# cursor.executemany(insert_query, users)
 
-select_query = "SELECT username, password FROM users"
+select_query = "SELECT username, password FROM users where username='gold'"
 for row in cursor.execute(select_query):
   print(row)
+
+
 
 # commiting the changes to database
 connection.commit()
