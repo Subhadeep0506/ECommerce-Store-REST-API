af8d3cbdc7a314b6e56c64eec51f4276582784d0
Subhadeep <subhadeepdoublecap@gmail.com>
2022-03-05 14:31:05 +0530

    Minor changes

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -3,21 +3,29 @@ from typing import List
 from requests import Response, post
 
 
+class MailgunException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
 class Mailgun:
 
-    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
-    MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
-
+    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")  # can be None
+    MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")  # can be None
     FROM_TITLE = "Stores RestAPI"
-
     # FROM_EMAIL = "Your Mailgun Email"
     FROM_EMAIL = "subhadeepdoublecap@gmail.com"
 
     # This method will interact with Mailgun API and return the response sent
     @classmethod
     def send_email(cls, email: List[str], subject: str, text: str, html: str) -> Response:
+        if cls.MAILGUN_API_KEY is None:
+            raise MailgunException("Falied to load Mailgun API key.")
 
-        return post(
+        if cls.MAILGUN_DOMAIN is None:
+            raise MailgunException("Failde to load Mailgun domain.")
+
+        response = post(
             f"https://api.mailgun.net/v3/{cls.MAILGUN_DOMAIN}/messages",
             auth=(
                 "api",
@@ -31,3 +39,8 @@ class Mailgun:
                 "html": html,
             },
         )
+
+        if response.status_code != 200:
+            raise MailgunException("Error in sending confirmation email, user registration failed.")
+
+        return response
